6a1946ab4da6cf9781ce455c073b4748feff9623
joaopugsley <joaopugsleyy@gmail.com>
2024-04-17 19:04:04 -0300

    feat: add wear and category filter

--- src/app/sticker/page.tsx
+++ src/app/sticker/page.tsx
@@ -8,6 +8,7 @@ import { Weapon } from "@/types/weapon.type";
 
 export default function StickerSearch() {
 
+  // data
   const [stickerData, setStickerData] = useState<Sticker[]>([]);
   const [weaponData, setWeaponData] = useState<Weapon[]>([]);
 
@@ -30,9 +31,13 @@ export default function StickerSearch() {
     return stickerData.map(sticker => ({ ...sticker, name: sticker.name.replace("Sticker | ", "")}));
   }, [stickerData]);
 
-  const [currentWeapon, setCurrentWeapon] = useState<string>("any");
+  // filters
+  const [weaponFilter, setWeaponFilter] = useState<string>("any");
+  const [wearFilter, setWearFilter] = useState<string>("any");
+  const [categoryFilter, setCategoryFilter] = useState<string>("any");
   const [ignoreStickerOrder, setIgnoreStickerOrder] = useState<boolean>(false);
 
+  // current stickers
   const [currentStickers, setCurrentStickers] = useState<Record<number, Sticker | undefined>>([]);
 
   const updateSticker = (slot: number, sticker?: Sticker) => {
@@ -42,13 +47,40 @@ export default function StickerSearch() {
     }))
   }
 
-  const handleButtonClick = () => {
+  const buildSearchLink = () => {
     const encodedStickers = Object.entries(currentStickers)
       .filter(([_, sticker]) => sticker !== undefined)
       .map(([_, sticker]) => sticker && encodeURI(sticker.name))
       .join(ignoreStickerOrder ? `"+"` : `,`);
+    
+    let searchLink = `https://steamcommunity.com/market/search?`;
 
-    const searchLink = `https://steamcommunity.com/market/search?q="${encodedStickers}"&descriptions=1&category_730_ItemSet%5B%5D=any&category_730_Weapon%5B%5D=${currentWeapon}&category_730_Quality%5B%5D=#p1_price_asc`;
+    // stickers
+    if (encodedStickers !== "") {
+      searchLink += `q="${encodedStickers}"&`;
+    }
+
+    // necessary query parameters
+    searchLink += "descriptions=1&category_730_ItemSet%5B%5D=any";
+
+    // weapon filter
+    searchLink += `&category_730_Weapon%5B%5D=${weaponFilter}`;
+
+    // wear filter
+    if (wearFilter !== "any") {
+      searchLink += `&category_730_Exterior%5B%5D=${wearFilter}`;
+    }
+
+    // category filter
+    if (categoryFilter !== "any") {
+      searchLink += `&category_730_Quality%5B%5D=${categoryFilter}`;
+    }
+
+    return searchLink;
+  };
+
+  const handleButtonClick = () => {
+    const searchLink = buildSearchLink();
     window.open(searchLink, "_blank", "noopener,noreferrer");
   };
 
@@ -60,8 +92,8 @@ export default function StickerSearch() {
         <select 
           name="weapon" 
           className="w-full bg-transparent px-2 py-2 rounded-md border border-zinc-800 outline-none"
-          value={currentWeapon}
-          onChange={(e) => setCurrentWeapon(e.target.value)}
+          value={weaponFilter}
+          onChange={(e) => setWeaponFilter(e.target.value)}
         >
           <option value="any" className="bg-zinc-900 text-white text-sm">Any Weapon</option>
           <option className="bg-zinc-900 text-white" disabled>--- Rifles</option>
@@ -85,6 +117,34 @@ export default function StickerSearch() {
             ))
           }
         </select>
+        <div className="grid grid-cols-2 gap-x-2 gap-y-1.5">
+          <h2 className="text-sm bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400 text-transparent">Wear</h2>
+          <h2 className="text-sm bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400 text-transparent">Category</h2>
+          <select 
+            name="Wear"
+            className="w-full bg-transparent px-2 py-2 rounded-md border border-zinc-800 outline-none"
+            value={wearFilter}
+            onChange={(e) => setWearFilter(e.target.value)}
+          >
+            <option value="any" className="bg-zinc-900 text-white text-sm">Any Wear</option>
+            <option value="tag_WearCategory0" className="bg-zinc-900 text-white text-sm">Factory New</option>
+            <option value="tag_WearCategory1" className="bg-zinc-900 text-white text-sm">Minimal Wear</option>
+            <option value="tag_WearCategory2" className="bg-zinc-900 text-white text-sm">Field Tested</option>
+            <option value="tag_WearCategory3" className="bg-zinc-900 text-white text-sm">Well Worn</option>
+            <option value="tag_WearCategory4" className="bg-zinc-900 text-white text-sm">Battle Scarred</option>
+          </select>
+          <select 
+            name="Category"
+            className="w-full bg-transparent px-2 py-2 rounded-md border border-zinc-800 outline-none"
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+          >
+            <option value="any" className="bg-zinc-900 text-white text-sm">Any Category</option>
+            <option value="tag_normal" className="bg-zinc-900 text-white text-sm">Normal</option>
+            <option value="tag_strange" className="bg-zinc-900 text-white text-sm">StatTrak™</option>
+            <option value="tag_tournament" className="bg-zinc-900 text-white text-sm">Souvenir</option>
+          </select>
+        </div>
         <div className="w-full flex flex-col space-y-4">
           {
             [0, 1, 2, 3, 4].map(slot => (
